11c911c5590cd53e7accb05162f47a01ade7b549
EmperorKnight <jlpicado@uamv.edu.ni>
2025-05-12 11:35:59 -0600

    ejemplo2

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,3 +1,13 @@
-for i in range(1,4):
-    for j in range(1,4):
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i,j)
+
+i = 1
+j = 1
+
+while i < 4:
+    while j < 4:
         print(i,j)
+        j += 1
+    i += 1
+    j = 1
